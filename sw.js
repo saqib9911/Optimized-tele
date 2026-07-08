53bf29295ad7404827d1f18724e96346f5af550b
saqib9911 <saqibzaheersattirocklight@gmail.com>
2026-07-08 15:54:07 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'saqskyscoop-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './og-preview.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
